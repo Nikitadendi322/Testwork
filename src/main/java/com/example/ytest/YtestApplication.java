c46f9d08bb59619b4dc03882b483b8b5886b858d
Никита <Ployka322@yandex.ru>
2023-08-06 10:16:31 +0700

    Start Test Work

---
+++ src/main/java/com/example/ytest/YtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ytest/YtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
